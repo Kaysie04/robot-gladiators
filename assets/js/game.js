17e06376e77d15ecd70ceb7aeeb2a6d64b1ac188
Kaysie <kaysielandersonwork@gmail.com>
2022-05-20 10:35:12 -0500

    fixed name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,7 +80,6 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-      debugger;
 
       // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
@@ -170,20 +169,16 @@ var shop = function() {
 
 /* GAME INFORMATION / VARIABLES */
 
-// function to set name
-var getPlayerName = function () {
-  var name= "";
-
+// player information
+//function to set name
+var getPlayerName = function() {
+  var name = "";
   while (name === "" || name === null) {
     name = prompt("What is your robot's name?");
   }
-
   console.log("Your robot's name is " + name);
-  return name;
 };
 
-
-// player information
 var playerInfo = {
   name: getPlayerName(),
   health: 100,
